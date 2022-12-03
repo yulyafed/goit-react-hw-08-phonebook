715cb2fb40da31713e748175448cfa4b578ec883
Yuliia Fedorychak <Yulyafed86@gmail.com>
2022-12-03 21:02:25 +0200

    Update Register.js

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,9 +1,18 @@
 import { Helmet } from 'react-helmet';
 import { RegisterForm } from 'components/RegisterForm/RegisterForm';
 
+const styles = {
+  container: {
+    minHeight: 'calc(100vh - 50px)',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+};
+  
 export default function Register() {
     return (
-        <div>
+        <div style={styles.container}>
             <Helmet>
                 <title>Registration</title>
             </Helmet>
